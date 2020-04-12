bc7c8f1986698ba1fe0560b002bb16b6d12df010
rigelr <rigelrahmawati@gmail.com>
2020-04-12 21:34:38 +0700

    add recipe model

--- app/src/main/java/id/putraprima/retrofit/api/services/ApiInterface.java
+++ app/src/main/java/id/putraprima/retrofit/api/services/ApiInterface.java
@@ -12,7 +12,7 @@ import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
-public interface ApiInterface{
+public interface  ApiInterface{
     @GET("/")
     Call<AppVersion> getAppVersion();
 
